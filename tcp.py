10f2a76795b0631f287c5fd95c96e0f0ebeb53b1
User5 <kingexotics01@hotmail.com>
2025-06-06 23:05:36 -0400

    Add files via upload

---
+++ tcp.py
@@ -0,0 +1,18 @@
+# tcp.py (Simple TCP server)
+import socket
+
+HOST = '127.0.0.1'  # localhost
+PORT = 65432        # arbitrary non-privileged port
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Listening on {HOST}:{PORT}")
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
